7d2dacbaca76a201096ce12f172efb7aa1304924
Tanmay <tanmay.bora221@vit.edu>
2026-01-16 19:56:06 +0530

    change upload button textx

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -179,7 +179,7 @@ const ContactUs = () => {
                                             fullWidth
                                             sx={{ py: 1.5, borderRadius: 2, borderStyle: 'dashed' }}
                                         >
-                                            {image ? `Selected: ${image.name}` : "Upload Image (Optional)"}
+                                            {image ? `Selected: ${image.name}` : "Upload Your DeviceImage (Optional)"}
                                             <input
                                                 type="file"
                                                 hidden
