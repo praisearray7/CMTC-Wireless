92385b9335da2bfc13cfd9b1eb4ec660421d9d76
Tanmay <tanmay.bora221@vit.edu>
2026-01-21 18:00:26 +0530

    add remaining images

--- src/data/smartwatch.ts
+++ src/data/smartwatch.ts
@@ -11,7 +11,7 @@ export const smartwatchData: SeriesData[] = [
         id: 'apple-watch',
         title: 'Apple Watch Repair',
         subtitle: 'Apple Watch Series Repair',
-        image: 'https://www.gophermods.com/wp-content/uploads/2020/04/Apple-Watch-Repair-200x200-1.jpg',
+        image: 'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/watch-card-40-hermes-ultra-202509_GEO_US?wid=680&hei=528&fmt=jpeg&qlt=90&.v=Ly93VWF6a1dGOWJLL3RMM0s0eGZ6bGptdm4xZHhxZWZzUlhoOU9Da0hNNVU4aHdGN0xlWGtoZjR6dnFUWE9VVTV0VzZXemQ1ZkRzK0p5ZFBxZERkQ3o2K3c3eDN1QlVKV09nQzhyNmV5TTNjeFVjd0E0NEk3ZEplNUNxd0pRazY',
         description: [
             "Expert repair for all Apple Watch Series. Series 9, Ultra, SE, and older models.",
             "Screen replacement, battery replacement, and more."
@@ -21,7 +21,7 @@ export const smartwatchData: SeriesData[] = [
         id: 'galaxy-watch',
         title: 'Galaxy Watch Repair',
         subtitle: 'Samsung Galaxy Watch Repair',
-        image: 'https://www.gophermods.com/wp-content/uploads/2020/04/Samsung-Galaxy-Watch-Repair-200x200-1.jpg',
+        image: 'https://images.samsung.com/is/image/samsung/p6pim/in/2407/gallery/in-galaxy-watch7-l310-sm-l310nzsains-542366980?$684_547_PNG$',
         description: [
             " repairs for Samsung Galaxy Watch 6, 5, 4, and Classic models.",
             "Fix broken screens, battery drain issues, and water damage."
@@ -31,7 +31,7 @@ export const smartwatchData: SeriesData[] = [
         id: 'pixel-watch',
         title: 'Pixel Watch Repair',
         subtitle: 'Google Pixel Watch Repair',
-        image: 'https://www.gophermods.com/wp-content/uploads/2020/04/Google-Pixel-Watch-Repair-200x200-1.jpg',
+        image: '/GooglePixelWatch.png',
         description: [
             "Google Pixel Watch repair services. Cracked screen? We can fix it.",
             "Fast and reliable service for your Pixel wearable."
