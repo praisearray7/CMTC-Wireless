b4b78459a09b4417c333d74db49d200594b63ce6
Tanmay <tanmay.bora221@vit.edu>
2026-01-26 21:41:38 +0530

    Updates

--- assets/tablet-BiF_0WLE.js
+++
@@ -1,1 +0,0 @@
-import{ad as e}from"./index-VWzq69vU.js";const t=[["rect",{width:"16",height:"20",x:"4",y:"2",rx:"2",ry:"2",key:"76otgf"}],["line",{x1:"12",x2:"12.01",y1:"18",y2:"18",key:"1dp563"}]],c=e("tablet",t);export{c as T};
