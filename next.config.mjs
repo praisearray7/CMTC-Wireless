a305e3593815d507335d2165bdb78a6d652d4e5b
Tanmay <tanmay.bora221@vit.edu>
2026-01-28 12:00:16 +0530

    fix: explicit any lints and allowed image domains

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,23 @@ const nextConfig = {
         formats: ['image/avif', 'image/webp'],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'guide-images.cdn.ifixit.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cart-products.cdn.ifixit.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.gophermods.com',
+                pathname: '/**',
+            },
+        ],
     },
     // Performance optimizations
     compress: true,
